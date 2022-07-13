8d12d0b0d1b180647dd7d1df0d929416c73faefb
Will Olson <will.olson11@gmail.com>
2022-07-12 18:58:17 -0700

    Add javascript to handle websockets form

---
+++ client/app.js
@@ -0,0 +1,25 @@
+$(function() {
+  let websocket = new WebSocket('wss://localhost:4444');
+  let room = $('#chat-room');
+
+  websocket.addEventListener('message', function(event) {
+    let data = JSON.parse(event.data);
+    let message = `<p><strong>${data.username}</strong>: ${data.text}</p>`;
+    
+    room.append(message);
+    room.scrollTop = room.scrollHeight;
+  });
+
+  $('#chat-form').submit(function(event) {
+    event.preventDefault();
+    let username = $('#input-username').val();
+    let message = $('#input-message').val();
+    
+    websocket.send(JSON.stringify({
+      username: username,
+      text: message,
+    }));
+    
+    $('#input-message').val('');
+  });
+});
