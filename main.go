69dd7a68ecd0a10edf4b4ce1dec1635ee2100e9a
Will Olson <will.olson11@gmail.com>
2022-07-13 14:11:00 -0700

    Make messages persistant

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/websocket"
 )
 
@@ -12,6 +14,7 @@ type ChatMessage struct {
 	Text     string `json:"text"`
 }
 
+var rdb *redis.Client
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -29,6 +32,8 @@ func websocketEndpoint(w http.ResponseWriter, r *http.Request) {
 
 	defer conn.Close()
 	clients[conn] = true
+
+	sendPreviousMessages(conn)
 	reader(conn)
 }
 
@@ -41,24 +46,70 @@ func reader(conn *websocket.Conn) {
 			return
 		}
 
-		// Broadcast message to all connected clients
 		log.Println("Received message: ", string(chatMessage.Text))
 		broadcast <- chatMessage
+	}
+}
+
+func sendPreviousMessages(conn *websocket.Conn) {
+	messages, err := rdb.LRange("messages", 0, -1).Result()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, message := range messages {
+		var chatMessage ChatMessage
+		err := json.Unmarshal([]byte(message), &chatMessage)
+		if err != nil {
+			panic(err)
+		}
+
+		messageClient(conn, chatMessage)
 	}
 }
 
 func handleMessages() {
 	for {
 		chatMessage := <-broadcast
-		for client := range clients {
-			err := client.WriteJSON(chatMessage)
-			if err != nil {
-				log.Println(err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
+
+		storeMessageInDatabase(chatMessage)
+		messageClients(chatMessage)
 	}
+}
+
+func storeMessageInDatabase(message ChatMessage) {
+	jsonMessage, err := json.Marshal(message)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := rdb.RPush("messages", jsonMessage).Err(); err != nil {
+		panic(err)
+	}
+}
+
+func messageClients(message ChatMessage) {
+	for client := range clients {
+		messageClient(client, message)
+	}
+}
+
+func messageClient(conn *websocket.Conn, message ChatMessage) {
+	err := conn.WriteJSON(message)
+	if err != nil {
+		log.Println(err)
+		conn.Close()
+		delete(clients, conn)
+	}
+}
+
+func setupDatabase() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 }
 
 func setupRoutes() {
@@ -69,6 +120,7 @@ func setupRoutes() {
 func main() {
 	log.Println("Starting http server...")
 
+	setupDatabase()
 	setupRoutes()
 	go handleMessages()
 
